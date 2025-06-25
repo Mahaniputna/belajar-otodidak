8aa30185f837468e2fb009806abe5f7a8e4648e0
Mahaniputna <masniceguy.official@gmail.com>
2025-06-25 22:08:32 +0700

    Add files via upload

---
+++ latihan.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kaum Otodidak</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#"> <img src = "https://static.vecteezy.com/system/resources/previews/006/988/698/non_2x/man-headphones-mascot-logo-gaming-illustration-vector.jpg" width="23px"></a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Dashboard</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="latihan.php?page=data_mahasiswa">Data Mahasiswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="latihan.php?page=input_data">Input Data</a>
+    </li>
+  </ul>
+</nav>
+<?php
+include 'libs.php';
+?>
+
+</body>
+</html>
